ca2de3b9c78dca129c4f381e4af76e18a5109c3c
Nathan Daniels <dadbeh@gmail.com>
2025-04-08 00:40:23 -0500

    Delete video_tools.py

--- video_tools.py
+++
@@ -1,7 +0,0 @@
-
-def generate_video_script(topic):
-    return f"""
-Hi everyone! In today's video, we're diving into {topic}. We'll walk through strategies, tips, and how you can win online with smart SEO.
-
-Don't forget to like and subscribe!
-"""
