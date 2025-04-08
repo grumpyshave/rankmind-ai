705b1749b3aa3533216bdb68d474c16612dc86c3
Nathan Daniels <dadbeh@gmail.com>
2025-04-08 00:40:03 -0500

    Delete blog_tools.py

--- blog_tools.py
+++
@@ -1,3 +0,0 @@
-
-def generate_blog(topic):
-    return f"# Blog Title: Boost Your SEO for {topic}\n\nWelcome to your RankMind-generated post. This blog covers the top tips for {topic}. Stay tuned for more."
